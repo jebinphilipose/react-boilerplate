991e3f9ae4674786288ba88578973578ec4c8ff7
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-14 12:08:27 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import your app here
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+// render your app here
+ReactDOM.render(<p>React Boilerplate</p>, document.getElementById('app'));
